a86ba694474b9300bdc00a805191c9398b39b34c
n.gnato <n.gnato@ya.ru>
2025-11-09 17:29:37 +0300

    Put removed from sdk logic

--- src/FreeElephants/LogTracer/Sentry/SentryTraceProviderVersionFrom4_12.php
+++ src/FreeElephants/LogTracer/Sentry/SentryTraceProviderVersionFrom4_12.php
@@ -4,15 +4,41 @@ declare(strict_types=1);
 
 namespace FreeElephants\LogTracer\Sentry;
 
-use FreeElephants\LogTracer\Exception\NotImplementedException;
+use Sentry\SentrySdk;
+use Sentry\State\Scope;
 
 class SentryTraceProviderVersionFrom4_12 extends AbstractSentryTraceProvider
 {
+    /**
+     * @see https://github.com/getsentry/sentry-php/pull/1833
+     */
     public function getTranceparentHeader(): string
     {
-        // \Sentry\getW3CTraceparent() is deprecated in senty/sentry 4.12
-        // see https://github.com/getsentry/sentry-php/pull/1833
-        // TODO: build this value here from other parts, handled by sdk?
-        throw new NotImplementedException();
+        $hub = SentrySdk::getCurrentHub();
+        $client = $hub->getClient();
+
+        if ($client !== null) {
+            $options = $client->getOptions();
+
+            if ($options->isTracingEnabled()) {
+                $span = SentrySdk::getCurrentHub()->getSpan();
+                if ($span !== null) {
+                    $sampled = '';
+
+                    if ($span->getSampled() !== null) {
+                        $sampled = $span->getSampled() ? '-01' : '-00';
+                    }
+
+                    return sprintf('00-%s-%s%s', $span->getTraceId(), $span->getSpanId(), $sampled);
+                }
+            }
+        }
+
+        $traceParent = '';
+        $hub->configureScope(function (Scope $scope) use (&$traceParent) {
+            $traceParent = sprintf('00-%s-%s', $scope->getPropagationContext()->getTraceId(), $scope->getPropagationContext()->getSpanId());
+        });
+
+        return $traceParent;
     }
 }
